450361e523e1635c6b2726ee506be3d7aa50c1fc
Eric Hutabarat <76461464+erichutabarat@users.noreply.github.com>
2024-06-09 12:19:27 +0700

    Features: Routes (Update)
    Implement dynamic import to routes and app

--- galeri-nusantara-frontend-main/src/routes/routes.js
+++ galeri-nusantara-frontend-main/src/routes/routes.js
@@ -1,23 +1,38 @@
-import aboutuspage from "../views/pages/aboutuspage";
-import detailpage from "../views/pages/detail";
-import homepage from "../views/pages/homepage";
-import favoritepage from "../views/pages/favorite";
-import loginpage from "../views/pages/loginpage";
-import registerpage from "../views/pages/registerpage";
-import dashboardPage from "../views/pages/dashboard";
-import updatepostpage from "../views/pages/updatepostpage";
-import homeAfter from "../views/after-render/home-after";
-import dashboardAfter from "../views/after-render/dashboard-after";
-
 const routes = [
-  { path: "#/", handler: homepage, afterRender: homeAfter},
-  { path: "#/detail", handler: detailpage },
-  { path: "#/favorite", handler: favoritepage },
-  { path: "#/about", handler: aboutuspage },
-  { path: "#/login", handler: loginpage },
-  { path: "#/register", handler: registerpage },
-  { path: "#/dashboard", handler: dashboardPage, afterRender: dashboardAfter },
-  { path: "#/update-postingan", handler: updatepostpage },
+  {
+    path: "#/",
+    handler: () => import("../views/pages/homepage"),
+    afterRender: () => import("../views/after-render/home-after")
+  },
+  {
+    path: "#/detail",
+    handler: () => import("../views/pages/detail")
+  },
+  {
+    path: "#/favorite",
+    handler: () => import("../views/pages/favorite")
+  },
+  {
+    path: "#/about",
+    handler: () => import("../views/pages/aboutuspage")
+  },
+  {
+    path: "#/login",
+    handler: () => import("../views/pages/loginpage")
+  },
+  {
+    path: "#/register",
+    handler: () => import("../views/pages/registerpage")
+  },
+  {
+    path: "#/dashboard",
+    handler: () => import("../views/pages/dashboard"),
+    afterRender: () => import("../views/after-render/dashboard-after")
+  },
+  {
+    path: "#/update-postingan",
+    handler: () => import("../views/pages/updatepostpage")
+  },
 ];
 
 export default routes;
--- galeri-nusantara-frontend-main/src/scripts/app.js
+++ galeri-nusantara-frontend-main/src/scripts/app.js
@@ -1,54 +1,60 @@
 import routes from "../routes/routes";
 import errorpage from "../views/pages/errorpage";
 
-class App{
-    path;
-    constructor(){
-        this.path =  window.location.hash;
-        this.appLocation = document.querySelector('#app');
-    }
-    async navigate(){
-        this.getPath();
-        const route = routes.find(route => route.path === this.path);
-        if(route){
-            if(route.path.includes('detail')){
-                const id = this.id;
-                const content = await route.handler(id);
-                await this.show(content);
-            }
-            else{
-                const content = await route.handler();
-                await this.show(content);
-            }
-            if(route.afterRender){
-                await route.afterRender();
-            }
-        }
-        else{
-            this.show(errorpage());
-        }
-    }
-    getPath(){
-        const allpath = window.location.hash || '#/';
-        if(allpath.includes('detail')){
-            const firstpath = allpath.split('/').slice(0, -1).join('/');
-            this.path = firstpath;
-            this.id = allpath.split('/detail/')[1];
-        }
-        else{
-            this.path = allpath;
-        }
-    }
-    
-    async show(content){
-        this.appLocation.innerHTML = '';
-        if (content) {
-            this.appLocation.innerHTML = content;
+class App {
+  path;
+  constructor() {
+    this.path = window.location.hash;
+    this.appLocation = document.querySelector('#app');
+  }
+  async navigate() {
+    this.getPath();
+    const route = routes.find(route => route.path === this.path);
+    if (route) {
+      try {
+        const handlerModule = await route.handler();
+        const handler = handlerModule.default;
+        let content;
+        if (route.path.includes('detail')) {
+          const id = this.id;
+          content = await handler(id);
         } else {
-            this.appLocation.innerHTML = "Content not found.";
+          content = await handler();
         }
+        await this.show(content);
+
+        if (route.afterRender) {
+          const afterRenderModule = await route.afterRender();
+          const afterRender = afterRenderModule.default;
+          await afterRender();
+        }
+      } catch (error) {
+        console.error('Error loading route handler:', error);
+        this.show(errorpage());
+      }
+    } else {
+      this.show(errorpage());
     }
-    
+  }
+  getPath() {
+    const allpath = window.location.hash || '#/';
+    if (allpath.includes('detail')) {
+      const firstpath = allpath.split('/').slice(0, -1).join('/');
+      this.path = firstpath;
+      this.id = allpath.split('/detail/')[1];
+    } else {
+      this.path = allpath;
+    }
+  }
+
+  async show(content) {
+    this.appLocation.innerHTML = '';
+    if (content) {
+      this.appLocation.innerHTML = content;
+    } else {
+      this.appLocation.innerHTML = "Content not found.";
+    }
+  }
 }
 
 export default App;
